f04b5806b25d6c72c3ad5a53964561b653561df4
sambaosman <contactsalmaosman@gmail.com>
2022-03-01 09:47:31 -0500

    fixing delete functionality

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,34 +1,38 @@
 import React, { useEffect, useState } from "react";
 import AdminPage from "./AdminPage";
 import PlayerPage from "./PlayerPage";
-import ManagerPage from "./ManagerPage";
 import { Auth } from "aws-amplify";
 import { TeamMember } from "./models";
 import { DataStore } from "aws-amplify";
 
 const HomePage = ({ teams, setTeams, setLoggedIn }) => {
   const [teamMembers, setTeamMembers] = useState([]);
+  const [userType, setUserType] = useState("");
+  const [teamID, setTeamID] = useState("");
+
   useEffect(() => {
     Auth.currentAuthenticatedUser().then((user) => {
-      console.log("user", user);
+      setUserType(user.attributes["custom:userType"]);
+      setTeamID(user.attributes["custom:teamID"]);
     });
-
-    const getTeamMembers = async () => {
-      let models = await DataStore.query(TeamMember);
-      setTeamMembers(models);
-    };
-    getTeamMembers();
   }, []);
 
   return (
     <React.Fragment>
-      {teamMembers.map((teamMember) => (
-        <div>
-          {teamMember.firstName}
-          {teamMember.teamsID}
-        </div>
-      ))}
-      <AdminPage teams={teams} setTeams={setTeams} setLoggedIn={setLoggedIn} />
+      {userType === "admin" ? (
+        <AdminPage
+          teams={teams}
+          setTeams={setTeams}
+          setLoggedIn={setLoggedIn}
+        />
+      ) : (
+        <PlayerPage
+          teamMembers={teamMembers}
+          setTeamMembers={setTeamMembers}
+          userType={userType}
+          teamID={teamID}
+        />
+      )}
     </React.Fragment>
   );
 };
--- src/PlayerPage.js
+++ src/PlayerPage.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { TeamMember } from "./models";
+import { DataStore } from "aws-amplify";
+import { deleteTeamMember } from "./RegistrationServices";
 
-const PlayerPage = () => {
-  return <div>Hello Manager</div>;
+const PlayerPage = ({ teamMembers, setTeamMembers, teamID }) => {
+  useEffect(() => {
+    const getTeamMembers = async () => {
+      let models = await DataStore.query(TeamMember);
+      setTeamMembers(models);
+    };
+    getTeamMembers();
+  }, []);
+
+  let filteredTeamMembers = teamMembers.filter(
+    (team) => team.teamsID === teamID
+  );
+  console.log("filter", filteredTeamMembers);
+
+  return (
+    <React.Fragment>
+      test
+      {teamMembers
+        // .filter((team) => team.teamsID === teamID)
+        .map((teamMember) => (
+          <div>
+            {teamMember.firstName}
+            {teamMember.teamsID}
+            <div onClick={() => deleteTeamMember(teamMember, setTeamMembers)}>
+              click here
+            </div>
+            C
+          </div>
+        ))}
+    </React.Fragment>
+  );
 };
 
 export default PlayerPage;
