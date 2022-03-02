9d2eabc627bf0bb9dc361621634ddcebabdbfc12
sambaosman <contactsalmaosman@gmail.com>
2022-03-02 14:34:32 -0500

    adding delete button for managers or admins

--- src/PlayerPage.js
+++ src/PlayerPage.js
@@ -3,6 +3,7 @@ import { TeamMember } from "./models";
 import { DataStore } from "aws-amplify";
 import { deleteTeamMember } from "./RegistrationServices";
 import { signOut } from "./LoginRegistration/LoginRegistrationFunctions";
+import { Row, Col } from "reactstrap";
 
 const PlayerPage = ({
   teamMembers,
@@ -11,71 +12,262 @@ const PlayerPage = ({
   setLoggedIn,
   userType,
 }) => {
+  const mockTeams = [
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "guestPlayer",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "guestPlayer",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "guestPlayer",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "player",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "player",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "player",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "player",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "player",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "player",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "manager",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "manager",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "manager",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "manager",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "manager",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "manager",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "manager",
+      teamsID: "1234",
+    },
+    {
+      firstName: "Salma",
+      lastName: "Osman",
+      jerseyNumber: "7",
+      teamMemberType: "manager",
+      teamsID: "1234",
+    },
+  ];
   useEffect(() => {
     const getTeamMembers = async () => {
       let models = await DataStore.query(TeamMember);
-      setTeamMembers(models);
+      setTeamMembers(mockTeams);
     };
     getTeamMembers();
   }, []);
 
   return (
-    <React.Fragment>
-      <div>Managers</div>
-      {teamMembers
-        .filter(
-          (team) => team.teamsID === teamID && team.teamMemberType === "manager"
-        )
-        .map((teamMember) => (
-          <div>
-            {teamMember.firstName}
-            {teamMember.teamsID}
-            {teamMember.teamMemberType}
+    <div style={{ width: "100%", margin: "auto" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: " space-between",
+          alignItems: "center",
+        }}
+      >
+        <div className="app-title">Team Roster</div>
+        <div className="logout-button" onClick={() => signOut(setLoggedIn)}>
+          {" "}
+          Log out
+        </div>
+      </div>
 
-            {userType === "manager" && (
-              <div onClick={() => deleteTeamMember(teamMember, setTeamMembers)}>
-                delete
-              </div>
-            )}
-          </div>
-        ))}
-      <div>Players</div>
-
-      {teamMembers
-        .filter(
-          (team) => team.teamsID === teamID && team.teamMemberType === "player"
-        )
-        .map((teamMember) => (
-          <div>
-            {teamMember.firstName}
-            {teamMember.teamsID}
-            {userType === "manager" && (
-              <div onClick={() => deleteTeamMember(teamMember, setTeamMembers)}>
-                delete
-              </div>
-            )}
-          </div>
-        ))}
-      <div>Guests</div>
+      <div className="roster-user-section">
+        <div className="roster-user-label">Managers</div>
+      </div>
       {teamMembers
         .filter(
           (team) =>
-            team.teamsID === teamID && team.teamMemberType === "guestPlayer"
+            team.teamsID === "1234" &&
+            // teamID
+            team.teamMemberType === "manager"
         )
-        .map((teamMember) => (
-          <div>
-            {teamMember.firstName}
-            {teamMember.teamsID}
-            {userType === "manager" && (
-              <div onClick={() => deleteTeamMember(teamMember, setTeamMembers)}>
-                delete
-              </div>
-            )}
-          </div>
+        .map((teamMember, index) => (
+          <RosterIndividual
+            key={index}
+            teamMember={teamMember}
+            setTeamMembers={setTeamMembers}
+            deleteTeamMember={deleteTeamMember}
+            userType={userType}
+          />
         ))}
-      <button onClick={() => signOut(setLoggedIn)}> Sign out</button>
-    </React.Fragment>
+      <div className="roster-user-section">
+        <div className="roster-user-label">Players</div>
+      </div>
+
+      {teamMembers
+        .filter(
+          (team) =>
+            team.teamsID === "1234" &&
+            // teamID
+            team.teamMemberType === "player"
+        )
+        .map((teamMember, index) => (
+          <RosterIndividual
+            key={index}
+            teamMember={teamMember}
+            setTeamMembers={setTeamMembers}
+            deleteTeamMember={deleteTeamMember}
+            userType={userType}
+          />
+        ))}
+      <div className="roster-user-section">
+        <div className="roster-user-label"> Guests</div>
+      </div>
+      {teamMembers
+        .filter(
+          (team) =>
+            team.teamsID === "1234" &&
+            // teamID
+            team.teamMemberType === "guestPlayer"
+        )
+        .map((teamMember, index) => (
+          <RosterIndividual
+            key={index}
+            teamMember={teamMember}
+            setTeamMembers={setTeamMembers}
+            deleteTeamMember={deleteTeamMember}
+            userType={userType}
+          />
+        ))}
+    </div>
   );
 };
 
 export default PlayerPage;
+
+const RosterIndividual = ({
+  teamMember,
+  setTeamMembers,
+  deleteTeamMember,
+  userType,
+}) => {
+  return (
+    <div style={{ display: "flex", justifyContent: "flex-start" }}>
+      <Row
+        style={{
+          margin: "20px",
+          display: "flex",
+          alignItems: "center",
+          width: "400px",
+        }}
+      >
+        <Col>
+          <div className="user-icon-circle">
+            {teamMember.teamMemberType === "manager" ? (
+              <i
+                className={`fa-solid fa-user-tie`}
+                style={{ fontSize: "26px" }}
+              />
+            ) : (
+              <div style={{ fontWeight: "bold" }}>
+                {teamMember.jerseyNumber}
+              </div>
+            )}
+          </div>
+        </Col>
+        <Col style={{ textAlign: "left", fontWeight: "bold" }}>
+          {teamMember.firstName} {teamMember.lastName}
+        </Col>
+        {userType === "manager" && (
+          <Col>
+            <div
+              className="delete-player-icon"
+              onClick={() => deleteTeamMember(teamMember, setTeamMembers)}
+            >
+              <i
+                className={`fa-solid fa-times`}
+                style={{ fontSize: "15px", color: "#a24936" }}
+              />
+            </div>
+          </Col>
+        )}
+      </Row>
+    </div>
+  );
+};
