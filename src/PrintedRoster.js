d231a01682cdc9b23df6f3871786c2e25d164e43
sambaosman <contactsalmaosman@gmail.com>
2022-03-06 18:11:11 -0500

    fixing printed roster changes

--- src/PrintedRoster.js
+++ src/PrintedRoster.js
@@ -14,81 +14,84 @@ class PrintedRoster extends React.Component {
           </span>
           <div className="player-header">Managers</div>
           <Table>
+            <thead>
+              <tr>
+                <th>Name</th>
+              </tr>
+            </thead>
             {managers && managers.length ? (
               managers.map((manager, index) => (
-                <React.Fragment>
-                  <thead>
-                    <tr>
-                      <th></th>
-                      <th>Name</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">{index + 1}</th>
-                      <td>
-                        {manager.firstName} {manager.lastName}
-                      </td>
-                    </tr>
-                  </tbody>
-                </React.Fragment>
+                <tbody key={index}>
+                  <tr>
+                    <td>
+                      {manager.firstName} {manager.lastName}
+                    </td>
+                  </tr>
+                </tbody>
               ))
             ) : (
-              <div>None</div>
+              <tbody>
+                <tr>
+                  <td>None</td>
+                  <td></td>
+                </tr>
+              </tbody>
             )}
           </Table>
           <div className="player-header">Players</div>
           <Table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Jersey Number</th>
+              </tr>
+            </thead>
             {players && players.length ? (
               players.map((player, index) => (
-                <React.Fragment key={index}>
-                  <thead>
-                    <tr>
-                      <th></th>
-                      <th>Name</th>
-                      <th>Jersey Number</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">{index + 1}</th>
-                      <td>
-                        {player.firstName} {player.lastName}{" "}
-                      </td>
-                      <td>{player.jerseyNumber}</td>
-                    </tr>
-                  </tbody>
-                </React.Fragment>
+                <tbody key={index}>
+                  <tr>
+                    <td>
+                      {player.firstName} {player.lastName}{" "}
+                    </td>
+                    <td>{player.jerseyNumber}</td>
+                  </tr>
+                </tbody>
               ))
             ) : (
-              <div>None</div>
+              <tbody>
+                <tr>
+                  <td>None</td>
+                  <td></td>
+                </tr>
+              </tbody>
             )}
           </Table>
           <div className="player-header">Guest Players</div>
           <Table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Jersey Number</th>
+              </tr>
+            </thead>
             {guestPlayers && guestPlayers.length ? (
-              guestPlayers.map((guest, index) => (
-                <React.Fragment key={index}>
-                  <thead>
-                    <tr>
-                      <th></th>
-                      <th>Name</th>
-                      <th>Jersey Number</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">{index + 1}</th>
-                      <td>
-                        {guest.firstName} {guest.lastName}{" "}
-                      </td>
-                      <td>{guest.jerseyNumber}</td>
-                    </tr>
-                  </tbody>
-                </React.Fragment>
+              guestPlayers.map((guestPlayer, index) => (
+                <tbody key={index}>
+                  <tr>
+                    <td>
+                      {guestPlayer.firstName} {guestPlayer.lastName}{" "}
+                    </td>
+                    <td>{guestPlayer.jerseyNumber}</td>
+                  </tr>
+                </tbody>
               ))
             ) : (
-              <div>None</div>
+              <tbody>
+                <tr>
+                  <td>None</td>
+                  <td></td>
+                </tr>
+              </tbody>
             )}
           </Table>
         </div>
