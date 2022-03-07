32028c96c73c23749759c2800ac0fcdc5e24948e
sambaosman <contactsalmaosman@gmail.com>
2022-03-07 10:16:44 -0500

    adding editing modal

--- src/EditTeamModal.js
+++ src/EditTeamModal.js
@@ -5,9 +5,10 @@ import {
   DropdownMenu,
   DropdownItem,
 } from "reactstrap";
-import { PrimaryButton, TextInput } from "./StyledComponents/StyledComponents";
-import { Form as ReactForm, FormGroup, Label, Modal } from "reactstrap";
+import { TextInput } from "./StyledComponents/StyledComponents";
+import { Form as ReactForm, FormGroup, Label } from "reactstrap";
 import { updateTeam } from "./TeamServices";
+import Modal from "./SharedComponents/Modal";
 
 const EditTeamModal = ({
   editTeamModalOpen,
@@ -23,52 +24,52 @@ const EditTeamModal = ({
 }) => {
   return (
     <Modal
-      isOpen={editTeamModalOpen}
-      toggle={() => setEditTeamModalOpen(false)}
-    >
-      <ReactForm style={{ padding: "20px" }}>
-        <FormGroup>
-          <Label for="firstName">Team Name</Label>
-          <TextInput
-            name="teamName"
-            id="teamName"
-            placeholder="Enter Team Name"
-            onChange={(event) => setTeamName(event.target.value)}
-          />
-          <Dropdown
-            isOpen={dropdownOpen}
-            toggle={() => setDropdownOpen(!dropdownOpen)}
-          >
-            <DropdownToggle
-              caret
-              style={{
-                backgroundColor: "white",
-                color: "black",
-                textTransform: "capitalize",
-              }}
+      isModalOpen={editTeamModalOpen}
+      toggleModal={() => setEditTeamModalOpen(false)}
+      title={"Shareable Links"}
+      isButtonVisible={true}
+      buttonFunction={(event) => {
+        updateTeam(event, editedTeam, teamName, setTeams, division);
+        setEditTeamModalOpen(false);
+      }}
+      buttonTitle={"Update Team"}
+      children={
+        <ReactForm>
+          <FormGroup>
+            <Label for="firstName">Team Name</Label>
+            <TextInput
+              name="teamName"
+              id="teamName"
+              placeholder="Enter Team Name"
+              onChange={(event) => setTeamName(event.target.value)}
+            />
+            <Dropdown
+              isOpen={dropdownOpen}
+              toggle={() => setDropdownOpen(!dropdownOpen)}
             >
-              {division ? division : "Division"}
-            </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem onClick={() => setDivision("premier")}>
-                Premier
-              </DropdownItem>
-              <DropdownItem onClick={() => setDivision("championship")}>
-                Championship
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </FormGroup>
-        <PrimaryButton
-          onClick={(event) => {
-            updateTeam(event, editedTeam, teamName, setTeams, division);
-            setEditTeamModalOpen(false);
-          }}
-        >
-          Update Team
-        </PrimaryButton>
-      </ReactForm>
-    </Modal>
+              <DropdownToggle
+                caret
+                style={{
+                  backgroundColor: "white",
+                  color: "black",
+                  textTransform: "capitalize",
+                }}
+              >
+                {division ? division : "Division"}
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem onClick={() => setDivision("premier")}>
+                  Premier
+                </DropdownItem>
+                <DropdownItem onClick={() => setDivision("championship")}>
+                  Championship
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </FormGroup>
+        </ReactForm>
+      }
+    />
   );
 };
 
