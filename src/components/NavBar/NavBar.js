e81b233c8cdcc68b4a0d120dffa8f23411be7f94
sambaosman <contactsalmaosman@gmail.com>
2022-03-15 17:50:51 -0400

    styling navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Nav, NavItem, NavLink, Row, Col } from "reactstrap";
 import "./NavBar.scss";
@@ -7,13 +7,13 @@ import logo from "../../Images/apllogo.png";
 
 const NavBar = () => {
   const history = useNavigate();
-
+  const [activeTab, setActiveTab] = useState(0);
   return (
     <div className="side-bar">
       <Row className="center">
-        <Col md="2">
+        <Col md="1">
           {" "}
-          <img src={logo} height={50} width={50} />
+          <img src={logo} height={40} width={40} className="grayscale" />
         </Col>
         <Col md="10">
           {" "}
@@ -22,17 +22,19 @@ const NavBar = () => {
       </Row>
       <Nav>
         {NavBarData.map((navItem, index) => (
-          <div className="nav-row">
-            <NavLink
-              href={navItem.link}
-              className="nav-option"
-              key={index}
-              active
-            >
-              {navItem.icon}
-              {navItem.title}
-            </NavLink>
-          </div>
+          <NavLink
+            href={navItem.link}
+            className={
+              activeTab !== index
+                ? "nav-option nav-link-container"
+                : "nav-option nav-link-container active"
+            }
+            key={index}
+            onClick={() => setActiveTab(index)}
+          >
+            {navItem.icon}
+            <span style={{ paddingLeft: "20px" }}>{navItem.title}</span>
+          </NavLink>
         ))}
       </Nav>
     </div>
