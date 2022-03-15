0e5c117e299e7620126ba30dac7b7afecb476c55
sambaosman <contactsalmaosman@gmail.com>
2022-03-15 16:25:54 -0400

    adding scss and importing google font poppins

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Nav, NavItem, NavLink } from "reactstrap";
+import "./NavBar.scss";
 
 const NavBar = () => {
   const history = useNavigate();
@@ -8,7 +9,9 @@ const NavBar = () => {
   return (
     <div>
       <Nav>
-        <NavLink href="#">Teams</NavLink>
+        <NavLink href="#" className="test">
+          Teams
+        </NavLink>
       </Nav>
     </div>
   );
