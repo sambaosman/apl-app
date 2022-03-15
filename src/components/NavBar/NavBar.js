581442af1ed11295b10be667e9cf81201c17722b
sambaosman <contactsalmaosman@gmail.com>
2022-03-15 17:24:46 -0400

    adding logo and title

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,39 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Nav, NavItem, NavLink } from "reactstrap";
+import { Nav, NavItem, NavLink, Row, Col } from "reactstrap";
 import "./NavBar.scss";
+import { NavBarData } from "./NavBarData";
+import logo from "../../Images/apllogo.png";
 
 const NavBar = () => {
   const history = useNavigate();
 
   return (
-    <div>
+    <div className="side-bar">
+      <Row className="center">
+        <Col md="2">
+          {" "}
+          <img src={logo} height={50} width={50} />
+        </Col>
+        <Col md="10">
+          {" "}
+          <div className="logo-title">American Premier League</div>
+        </Col>
+      </Row>
       <Nav>
-        <NavLink href="#" className="test">
-          Teams
-        </NavLink>
+        {NavBarData.map((navItem, index) => (
+          <div className="nav-row">
+            <NavLink
+              href={navItem.link}
+              className="nav-option"
+              key={index}
+              active
+            >
+              {navItem.icon}
+              {navItem.title}
+            </NavLink>
+          </div>
+        ))}
       </Nav>
     </div>
   );
