e4f3215e97ed2a10f6cef052b6fb3cfcdde6b2bd
sambaosman <contactsalmaosman@gmail.com>
2022-03-19 13:29:28 -0400

    adding form validation

--- src/components/AdminPage/EditAddTeamPanel/AddTeam.js
+++ src/components/AdminPage/EditAddTeamPanel/AddTeam.js
@@ -10,9 +10,8 @@ import { Form as ReactForm, FormGroup, Label } from "reactstrap";
 import { addTeam } from "../../../server/ApiFunctions";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
-import NavBar from "../../NavBar/NavBar";
+import { Form, Field } from "react-final-form";
 import { Row, Col } from "reactstrap";
-import { Icon } from "@iconify/react";
 
 import "./EditAddTeam.scss";
 
@@ -55,6 +54,10 @@ const AddTeam = ({
     }
   }, [file]);
 
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <Col className="edit-add-side-panel">
       <div className="page-container">
@@ -68,121 +71,189 @@ const AddTeam = ({
             }
           />
         </div>
-        <FormGroup>
-          <TextInput
-            name="teamName"
-            id="teamName"
-            placeholder="Enter Team Name"
-            onChange={(event) => setTeamName(event.target.value)}
-          />
-          <Dropdown
-            isOpen={dropdownOpen}
-            toggle={() => setDropdownOpen(!dropdownOpen)}
-          >
-            <StyledDropdownToggle
-              caret
-              className="dropdown-toggle"
-              style={{ backgroundColor: "#f8f8f8" }}
-            >
-              {division ? division : "Division"}
-            </StyledDropdownToggle>
-            <DropdownMenu>
-              <DropdownItem onClick={() => setDivision("Premier")}>
-                Premier
-              </DropdownItem>
-              <DropdownItem onClick={() => setDivision("Championship")}>
-                Championship
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-          <UploadButton
-            filename={file}
-            onChange={(e) => setFile(e.target.files[0])}
-            type="file"
-            accept="image/*"
-          />
-        </FormGroup>
-        <div className="review-text">Review:</div>
-        <Card className="review-team-card">
-          <Row style={{ width: "100%" }}>
-            <Col className="center" style={{ width: "10%", margin: "auto" }}>
-              <div className="team-logo">
-                {file ? (
-                  <img src={image} className="image-container" />
-                ) : (
-                  <div
-                    className="division-label "
-                    style={{ textAlign: "center", padding: "10px" }}
+        <Form
+          onSubmit={onSubmit}
+          validate={(values) => {
+            const errors = {};
+            if (!teamName) {
+              errors.teamName = "Required";
+            }
+            if (!division) {
+              errors.division = "Required";
+            }
+            if (!file) {
+              errors.file = "Required";
+            }
+            return errors;
+          }}
+          render={({ handleSubmit, values, submitting, validating, valid }) => (
+            <form onSubmit={handleSubmit}>
+              <FormGroup>
+                <Field name="teamName">
+                  {({ meta }) => (
+                    <div>
+                      <TextInput
+                        name="teamName"
+                        id="teamName"
+                        placeholder="Enter Team Name"
+                        onChange={(event) => setTeamName(event.target.value)}
+                      />
+                      {meta.error && meta.touched && (
+                        <span className="form-error">{meta.error}</span>
+                      )}
+                    </div>
+                  )}
+                </Field>
+              </FormGroup>
+              <FormGroup>
+                <Field name="division">
+                  {({ meta }) => (
+                    <div>
+                      <Dropdown
+                        isOpen={dropdownOpen}
+                        toggle={() => setDropdownOpen(!dropdownOpen)}
+                      >
+                        <StyledDropdownToggle
+                          caret
+                          className="dropdown-toggle"
+                          style={{ backgroundColor: "#f8f8f8" }}
+                        >
+                          {division ? division : "Division"}
+                        </StyledDropdownToggle>
+                        <DropdownMenu>
+                          <DropdownItem onClick={() => setDivision("Premier")}>
+                            Premier
+                          </DropdownItem>
+                          <DropdownItem
+                            onClick={() => setDivision("Championship")}
+                          >
+                            Championship
+                          </DropdownItem>
+                        </DropdownMenu>
+                      </Dropdown>
+                      {meta.error && meta.touched && (
+                        <span className="form-error">{meta.error}</span>
+                      )}
+                    </div>
+                  )}
+                </Field>
+              </FormGroup>
+              <FormGroup>
+                <Field name="file">
+                  {({ meta }) => (
+                    <div>
+                      <UploadButton
+                        filename={file}
+                        onChange={(e) => setFile(e.target.files[0])}
+                        type="file"
+                        accept="image/*"
+                      />
+                      {meta.error && meta.touched && (
+                        <span className="form-error">{meta.error}</span>
+                      )}
+                    </div>
+                  )}
+                </Field>
+              </FormGroup>
+              <div className="review-text">Review:</div>
+              <Card className="review-team-card">
+                <Row style={{ width: "100%" }}>
+                  <Col
+                    className="center"
+                    style={{ width: "10%", margin: "auto" }}
                   >
-                    Upload Logo
-                  </div>
-                )}
-              </div>
-            </Col>
-            <Col
-              className="flex-start"
-              style={{ width: "70%", margin: "auto" }}
-            >
-              <div>
-                <div
-                  className="team-card-title"
-                  style={{ paddingTop: "0px", textAlign: "left" }}
+                    <div className="team-logo">
+                      {file ? (
+                        <img src={image} className="image-container" />
+                      ) : (
+                        <div
+                          className="division-label "
+                          style={{ textAlign: "center", padding: "10px" }}
+                        >
+                          Upload Logo
+                        </div>
+                      )}
+                    </div>
+                  </Col>
+                  <Col
+                    className="flex-start"
+                    style={{ width: "70%", margin: "auto" }}
+                  >
+                    <div>
+                      <div
+                        className="team-card-title"
+                        style={{ paddingTop: "0px", textAlign: "left" }}
+                      >
+                        {" "}
+                        {teamName ? teamName : "Team Name"}{" "}
+                      </div>
+                      <div className="team-card-subtitle">
+                        {division ? division : "Division"}
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
+              </Card>
+              <Row
+                className="text-buttons"
+                style={{ margin: "auto !important" }}
+              >
+                <Col
+                  md="6"
+                  className="center"
+                  style={{ paddingRight: "5px", maxWidth: "50%" }}
                 >
-                  {" "}
-                  {teamName ? teamName : "Team Name"}{" "}
-                </div>
-                <div className="team-card-subtitle">
-                  {division ? division : "Division"}
-                </div>
-              </div>
-            </Col>
-          </Row>
-        </Card>
-        <Row className="text-buttons" style={{ margin: "auto !important" }}>
-          <Col
-            md="6"
-            className="center"
-            style={{ paddingRight: "5px", maxWidth: "50%" }}
-          >
-            <CardButtonWithText
-              style={{ backgroundColor: "var(--danger-transparent)" }}
-              onClick={(event) => {
-                setShowAddTeam(false);
-              }}
-            >
-              <span className="center">
-                <i
-                  className={`fa-solid fa-xmark`}
-                  style={{ fontSize: "15px", color: "var(--danger)" }}
-                />
-              </span>
-              <span className="button-title" style={{ color: "var(--danger)" }}>
-                Cancel
-              </span>
-            </CardButtonWithText>
-          </Col>
-          <Col
-            md="6"
-            className="center"
-            style={{ paddingLeft: "5px", maxWidth: "50%" }}
-          >
-            <CardButtonWithText
-              style={{ backgroundColor: "var(--secondary-transparent)" }}
-              onClick={(event) => {
-                event.stopPropagation();
-                submit(setTeamName, setDivision, showAddTeam, setShowAddTeam);
-              }}
-            >
-              <span className="center">
-                <i
-                  className={`fa-solid fa-plus`}
-                  style={{ fontSize: "15px", color: "var(--secondary)" }}
-                />
-              </span>
-              <span className="button-title">Add Team</span>
-            </CardButtonWithText>{" "}
-          </Col>
-        </Row>
+                  <CardButtonWithText
+                    style={{ backgroundColor: "var(--danger-transparent)" }}
+                    onClick={(event) => {
+                      setShowAddTeam(false);
+                    }}
+                  >
+                    <span className="center">
+                      <i
+                        className={`fa-solid fa-xmark`}
+                        style={{ fontSize: "15px", color: "var(--danger)" }}
+                      />
+                    </span>
+                    <span
+                      className="button-title"
+                      style={{ color: "var(--danger)" }}
+                    >
+                      Cancel
+                    </span>
+                  </CardButtonWithText>
+                </Col>
+                <Col
+                  md="6"
+                  className="center"
+                  style={{ paddingLeft: "5px", maxWidth: "50%" }}
+                >
+                  <CardButtonWithText
+                    style={{ backgroundColor: "var(--secondary-transparent)" }}
+                    disabled={!valid}
+                    onClick={(event) => {
+                      event.stopPropagation();
+                      submit(
+                        setTeamName,
+                        setDivision,
+                        showAddTeam,
+                        setShowAddTeam
+                      );
+                    }}
+                  >
+                    <span className="center">
+                      <i
+                        className={`fa-solid fa-plus`}
+                        style={{ fontSize: "15px", color: "var(--secondary)" }}
+                      />
+                    </span>
+                    <span className="button-title">Add Team</span>
+                  </CardButtonWithText>{" "}
+                </Col>
+              </Row>
+            </form>
+          )}
+        />
       </div>
     </Col>
 
