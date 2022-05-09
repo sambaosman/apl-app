c005324d67f4a1f076df112a367bef8115163419
sambaosman <contactsalmaosman@gmail.com>
2022-05-09 15:33:11 -0400

    adding multiple teams page

--- src/components/MyTeam/MultipleTeams.jsx
+++ src/components/MyTeam/MultipleTeams.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import AddTeam from "./EditAddTeamPanel/AddTeam";
+// import EditTeam from "./EditAddTeamPanel/EditTeam";
+// import "./AdminPage.scss";
+import TeamCard from "../AdminPage/TeamCard/TeamCard";
+import { Col } from "reactstrap";
+import { GoogleLogout } from "react-google-login";
+import { CardButtonWithText } from "../../StyledComponents/StyledComponents";
+import { useDispatch, useSelector } from "react-redux";
+import { clearUser } from "../../redux/userSlice";
+import { getTeamById } from "../../server/ApiFunctions";
+import RegistrationSelector from "../../LoginRegistration/Registration/RegistrationSelector";
 
-const MultipleTeams = () => {
-  return <div>test</div>;
+const MultipleTeams = ({ googleData }) => {
+  const [showInput, setShowInput] = useState(false);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.user);
+
+  useEffect(() => {
+    let teams = [];
+    user &&
+      user.teams &&
+      user.teams.length &&
+      user.teams.map((team) => {
+        getTeamById(team.id).then((team) => {
+          teams.push(team);
+        });
+      });
+    console.log("teams", teams);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Col className="right-column">
+        <div className="page-container">
+          <div className="page-signout-button">
+            <GoogleLogout
+              clientId={
+                "281501315717-3q4u5jr1fnil0eamk218j0bshq9tp8j6.apps.googleusercontent.com"
+              }
+              render={(renderProps) => (
+                <CardButtonWithText
+                  style={{
+                    backgroundColor: "white",
+                    width: "120px",
+                    display: "flex",
+                    justifyContent: "flex-end",
+                  }}
+                  onClick={renderProps.onClick}
+                >
+                  <i
+                    className="fa-solid fa-arrow-right-to-bracket"
+                    style={{ fontSize: "15px", color: "#4A4A4A" }}
+                  />
+                  <span style={{ color: "#4A4A4A", paddingLeft: "10px" }}>
+                    Sign out
+                  </span>
+                </CardButtonWithText>
+              )}
+              onLogoutSuccess={() => dispatch(clearUser())}
+              cookiePolicy={"single_host_origin"}
+              style={{ alignItems: "center" }}
+            />
+          </div>
+          <div className="admin-heading">
+            <div className="page-title">My Teams</div>
+            <div
+              className="add-circle-button"
+              onClick={() => setShowInput(true)}
+            >
+              <i
+                className={`fa-solid fa-plus`}
+                style={{ fontSize: "25px", color: "white" }}
+              />
+            </div>
+          </div>
+          <div className="grid-wrapper">
+            {/* {premierTeams && premierTeams.length
+              ? premierTeams.map((team) => (
+                  <TeamCard
+                    team={team}
+                    setTeams={setTeams}
+                    setShowEditTeam={setShowEditTeam}
+                    updateTeam={updateTeam}
+                    setClickedTeam={setClickedTeam}
+                    history={history}
+                  />
+                ))
+              : null} */}
+          </div>
+        </div>
+      </Col>
+      {showInput && <RegistrationSelector googleData={googleData} />}
+    </React.Fragment>
+  );
 };
 
 export default MultipleTeams;
